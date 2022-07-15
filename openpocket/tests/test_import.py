45784b4152e74dfac7ad642c4becc12ccda3b7c1
Diego Prada-Gracia <prada.gracia@gmail.com>
2022-07-14 21:02:22 -0500

    First structure of openpocket directory

---
+++ openpocket/tests/test_import.py
@@ -0,0 +1,13 @@
+"""
+Unit and regression test for the openpocket package.
+"""
+
+# Import package, test suite, and other packages as needed
+import openpocket
+import pytest
+import sys
+
+def test_openpocket_imported():
+    """Sample test, will always pass so long as import statement worked"""
+    assert "openpocket" in sys.modules
+
